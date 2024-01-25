9e7124280ac6b84a028c4b6054089ba18e3f071d
Tainna <tainnaandryelli@gmail.com>
2024-01-25 15:38:57 -0300

    ✨ feat:  imagens e perguntas frequentes dinamicas

--- js/script.js
+++ js/script.js
@@ -24,3 +24,47 @@ function ativarItem(parametro) {
 }
 
 parametros.forEach(ativarItem);
+
+// Ativando descrição de perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+
+  if (resposta.classList.contains("ativa")) {
+    pergunta.setAttribute("aria-expanded", "true");
+  } else {
+    pergunta.setAttribute("aria-expanded", "false");
+  }
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Imagens
+const galeria = document.querySelectorAll(".bicicleta-interna-imagens img");
+const principal = document.querySelector(".bicicleta-interna-imagens img");
+
+function mudaPrincipal(event) {
+  const clicada = event.currentTarget;
+  const srcPrincipal = principal.src;
+  const altPrincipal = principal.alt;
+
+  principal.src = clicada.src;
+  principal.alt = clicada.alt;
+
+  clicada.src = srcPrincipal;
+  clicada.alt = altPrincipal;
+}
+
+function selecionaImagem(image) {
+  image.addEventListener("click", mudaPrincipal);
+}
+galeria.forEach(selecionaImagem);
